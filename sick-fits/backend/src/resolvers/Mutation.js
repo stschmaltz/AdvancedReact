16f9b4ccbf35927957d8e689587840e4cca67f4b
Shane Schmaltz <stschamltz@gmail.com>
2018-11-11 11:05:36 -0700

    wrote the first query and mutation
    Used apollo, yoga and prisma

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if user is authenticated
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info,
+    );
+
+    return item;
+  },
+};
 
 module.exports = Mutations;
