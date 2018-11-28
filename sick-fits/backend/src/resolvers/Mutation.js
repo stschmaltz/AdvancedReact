9737a277142a8ab675ba87de5dfad59a2684c8b3
Shane Schmaltz <stschamltz@gmail.com>
2018-11-27 18:02:25 -0700

    hook up backend for signup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if user is authenticated
@@ -36,6 +39,38 @@ const Mutations = {
     // TODO
     // 3. delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+
+  async signup(parent, args, ctx, info) {
+    // to lower case the email
+    args.email = args.email.toLowerCase();
+
+    // hash password
+    const password = await bcrypt.hash(args.password, 10);
+
+    // create user in db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password: password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info,
+    );
+
+    //create jwt token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // set jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 24 * 365, // 1 year cookie
+    });
+
+    //return user to browser
+    return user;
   },
 };
 
