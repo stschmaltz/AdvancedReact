36e8ab1cf8f0932a0bb3ff9d3c18d56c538c59f1
Shane Schmaltz <stschamltz@gmail.com>
2018-11-27 18:41:19 -0700

    create render prop for currently logged in user

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 
@@ -9,7 +10,17 @@ const server = createServer();
 
 server.express.use(cookieParser());
 
-// TODO use express middleware to populate current user
+// decode jwt for user id on each req
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId on the the req for furture requests access
+    req.userId = userId;
+  }
+  console.log(token);
+  next();
+});
 
 server.start(
   {
